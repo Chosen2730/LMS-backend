7bbe7741158c42020cb3f85606bb09a8a53a5bc4
chosen2730 <cf.youvarsity@gmail.com>
2024-01-08 22:46:43 +0100

    added new changes

--- src/app.ts
+++ src/app.ts
@@ -16,17 +16,14 @@ import {
 } from "./routes";
 
 dotenv.config();
-const app = express();
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-    optionsSuccessStatus: 204,
-    allowedHeaders: "Content-Type,Authorization",
-  })
-);
 // Middlewares
+const app = express();
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
