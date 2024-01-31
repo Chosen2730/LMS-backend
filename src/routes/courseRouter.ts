f45dc1e71e2cc566502cd9ff09ec01ab8e858175
chosen2730 <cf.youvarsity@gmail.com>
2024-02-01 00:52:33 +0100

    added new change

--- src/routes/courseRouter.ts
+++ src/routes/courseRouter.ts
@@ -17,6 +17,6 @@ router.get("/tutor", authorize, getTutorCourses);
 router.get("/categories", authorize, getAllCategories);
 router.get("/enrol/:courseId", authorize, enrol);
 router.patch("/update-thumbnail/:courseId", authorize, updateCourseThumbnail);
-router.patch("/update-trailer/:courseId", authorize, updateTrailer);
+router.post("/update-trailer/:courseId", authorize, updateTrailer);
 
 export default router;
