bc816f8c23294cada6257491844d69bab4e891d9
chosen2730 <cf.youvarsity@gmail.com>
2024-02-01 00:47:30 +0100

    fixed another issue

--- src/app.ts
+++ src/app.ts
@@ -39,8 +39,10 @@ app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp" }));
 app.use(cors(corsOptions));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(
-  express.json({
-    limit: "100mb",
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
   })
 );
 app.use(
